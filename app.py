8e710a52f68c3a165f9a8b81c1e62ce92aaff291
FishMountain <145592033+FishMountain@users.noreply.github.com>
2023-09-24 03:22:55 +0000

    add DB connection

--- app.py
+++ app.py
@@ -1,42 +1,16 @@
+from database import load_jobs_from_db
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS=[
-  {
-    'id': 1,
-    'title':'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': 2,
-    'title':'Frontend Engineer',
-    'location': 'Remote',
-    'salary': 'Rs. 12,50,000'
-  },
-  {
-    'id': 3,
-    'title':'Data Scientist',
-    'location': 'Delhi, India',
-    
-  },
-  {
-    'id': 5,
-    'title':'Data Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$220,000'
-  },
-]
-
-
 @app.route("/")
 def hell_jovian():
-  return render_template('home.html', jobs=JOBS, company_name='Jovian')
+  jobs=load_jobs_from_db()
+  return render_template('home.html', jobs=jobs, company_name='Jovian')
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  return jsonify(load_jobs_from_db())
 
 if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,26 @@
+import os
+from sqlalchemy import create_engine, text
+
+
+db_connection_string=os.environ['DB_CONNECTION_STRING_JOVIAN_CAREERS_WEBSITE']
+
+engine = create_engine(db_connection_string,
+          connect_args={
+                  "ssl": {
+                      "ssl_ca": "/etc/ssl/cert.pem"
+                  }
+              })
+
+
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs=[]
+    for row in result.all():
+      jobs.append(row._asdict())
+    return jobs
+
+
+  
+
